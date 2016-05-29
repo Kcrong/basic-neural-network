739c3db50288a7b23efe38bc85b643ea312a2220
kcrong <kcrong@kim82536.pe.kr>
2016-05-30 08:55:20 +0900

    Change space sigmoid func

--- main.py
+++ main.py
@@ -3,8 +3,7 @@ from random import uniform, choice
 from math import exp
 
 
-def sigmoid(x):
-    return 1.0 / (1.0 + exp(-x))
+def sigmoid(x): return 1.0 / (1.0 + exp(-x))
 
 
 def randomkey(length):
