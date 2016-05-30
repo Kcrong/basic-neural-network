a1e4ecc910ea7c9dcd11e8c0a90eaf961dbf5790
kcrong <kcrong@kim82536.pe.kr>
2016-05-31 08:55:45 +0900

    Add learn, fix func

--- sigmoid.py
+++ sigmoid.py
@@ -1,34 +1,46 @@
 from math import exp
 from random import uniform
 
+INPUT_CNT = 2
+
 
 def sigmoid(x): return 1.0 / (1.0 + exp(-x))
 
 
 class Neuron:
-    def __init__(self, neuron_input=None):
-        self.input = neuron_input
+    def __init__(self):
         self.alpha = 0.1
-        if neuron_input:
-            self.input_weight = [uniform(-1, 1) for _ in range(len(self.input) + 1)]
-        else:
-            self.input_weight = list()
-        self.weight_error = list()
+        self.weight_error = [0 for _ in range(INPUT_CNT+1)]
+        self.weight = [uniform(-1, 1) for _ in range(INPUT_CNT+1)]
 
     def __repr__(self):
         return "<Neuron>"
 
-    def work(self):
+    def work(self, input_data):
         # return sigmoid(sum([self.input[i] * self.input_weight[i] for i in range(len(self.input))]))
 
-        sum = 0
+        output_sum = 0
 
-        for i in range(len(self.input)):
-            sum += self.input_weight[i] * self.input[i]
+        for idx in range(len(input_data)):
+            output_sum += self.weight[idx] * input_data[idx]
 
-        sum += self.input_weight[-1]
+        output_sum += self.weight[-1]
 
-        return sigmoid(sum)
+        return sigmoid(output_sum)
+
+    def learn(self, input_data, answer):
+        output = self.work(input_data)
+        output_error = output - answer
+
+        for idx in range(len(input_data)):
+            self.weight_error[idx] += output_error * input_data[idx] * output * (1 - output)
+
+        self.weight_error[-1] += output_error * 1.0 * output * (1 - output)
+
+    def fix(self):
+        for idx in range(INPUT_CNT+1):
+            self.weight[idx] -= self.alpha * self.weight_error[idx]
+            self.weight_error[idx] = 0
 
 
 if __name__ == '__main__':
@@ -41,6 +53,14 @@ if __name__ == '__main__':
 
     correct_input = [0, 0, 0, 1]
 
-    for data, correct_data in zip(all_input, correct_input):
-        neuron = Neuron(data)
-        print(neuron.work())
+    neuron = Neuron()
+
+    for i in range(40000):
+        for j in range(4):
+            neuron.learn(all_input[j], correct_input[j])
+        neuron.fix()
+
+        if (i + 1) % 1000 == 0:
+            print("-----------Learn %d times-----------" % (i+1))
+            for j in range(4):
+                print("%d %d : %f" % (all_input[j][0], all_input[j][1], neuron.work(all_input[j])))
