605739c29f18dc7f6d44b109552cf9d926a7fb58
kcrong <kcrong@kim82536.pe.kr>
2016-05-31 09:11:37 +0900

    Add comment Edit learn count

--- sigmoid.py
+++ sigmoid.py
@@ -9,9 +9,9 @@ def sigmoid(x): return 1.0 / (1.0 + exp(-x))
 
 class Neuron:
     def __init__(self):
-        self.alpha = 0.1
-        self.weight_error = [0 for _ in range(INPUT_CNT+1)]
-        self.weight = [uniform(-1, 1) for _ in range(INPUT_CNT+1)]
+        self.alpha = 0.1  # 민감도 상수
+        self.weight_error = [0 for _ in range(INPUT_CNT+1)]  # 가중치 오류를 저장할 리스트
+        self.weight = [uniform(-1, 1) for _ in range(INPUT_CNT+1)]  # 가중치 리스트
 
     def __repr__(self):
         return "<Neuron>"
@@ -29,18 +29,19 @@ class Neuron:
         return sigmoid(output_sum)
 
     def learn(self, input_data, answer):
-        output = self.work(input_data)
-        output_error = output - answer
+        output = self.work(input_data)  # 결과값 저장
+        output_error = output - answer  # 결과값에 대한 오류값 저장
 
         for idx in range(len(input_data)):
             self.weight_error[idx] += output_error * input_data[idx] * output * (1 - output)
+            # W_i <- W_i = Alpha ( E * X_i * O(1-O) )
 
-        self.weight_error[-1] += output_error * 1.0 * output * (1 - output)
+        self.weight_error[-1] += output_error * 1.0 * output * (1 - output)  # For Alpha
 
     def fix(self):
         for idx in range(INPUT_CNT+1):
-            self.weight[idx] -= self.alpha * self.weight_error[idx]
-            self.weight_error[idx] = 0
+            self.weight[idx] -= self.alpha * self.weight_error[idx]  # 민감도 상수를 이용해 가중치 교정
+            self.weight_error[idx] = 0  # 오류 저장 리스트 초기화
 
 
 if __name__ == '__main__':
@@ -55,12 +56,12 @@ if __name__ == '__main__':
 
     neuron = Neuron()
 
-    for i in range(40000):
+    for i in range(30000):
         for j in range(4):
             neuron.learn(all_input[j], correct_input[j])
         neuron.fix()
 
-        if (i + 1) % 1000 == 0:
-            print("-----------Learn %d times-----------" % (i+1))
+        if i % 2000 == 0:
+            print("-----------Learn %d times-----------" % i)
             for j in range(4):
                 print("%d %d : %f" % (all_input[j][0], all_input[j][1], neuron.work(all_input[j])))
